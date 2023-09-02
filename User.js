ca64fc7e15593ecd0c54248de71a93a1f006139f
unknown <bappy.raj.bd@gmail.com>
2023-09-02 11:03:07 +0600

    Models

---
+++ User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const CartItem = require('./CartItem');
+const Order = require('./Order');
+
+const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    address: { type: String },
+    phoneNumber: { type: String },
+  },
+  {versionKey:false}
+);
+
+// Relationship with CartItem: When a user is deleted, remove associated cart items.
+userSchema.pre('remove', async function (next) {
+  await CartItem.deleteMany({ user: this._id });
+  next();
+});
+
+// Relationship with Order: When a user is deleted, remove associated orders.
+userSchema.pre('remove', async function (next) {
+  await Order.deleteMany({ user: this._id });
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
