9d667fa94f6031ab98069b23fcdae73919023f76
Mehedi Newaz Sharif <106735946+mehedinewazsharif@users.noreply.github.com>
2023-09-02 11:20:12 +0600

    Update CartItem.js

--- CartItem.js
+++ CartItem.js
@@ -5,7 +5,7 @@ const cartItemSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   quantity: { type: Number, required: true, min: 1 },
 },
-  {versionKey:false}
+  { timestamps: true, versionKey:false}
 
 );
 
